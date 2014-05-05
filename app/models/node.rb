bd959d35cedd3a0330d444683f0e53841fba1200
Jimmy Lee <jdl39@stanford.edu>
2014-05-05 13:50:49 -0700

    Rating page now reflects the branch point.

--- app/models/node.rb
+++ app/models/node.rb
@@ -8,17 +8,28 @@ class Node < ActiveRecord::Base
   has_many :children, :class_name => "Node", :foreign_key => "parent_node_id"
   has_many :ratings
 
-  def render_to_text
-  	if self.parent_node.nil?
-  		return self.text
-  	else
-  		return self.parent_node.render_to_text + " " + self.text
-  	end
+  def render_to_text_after_node(node)
+    if self.parent_node == node
+      return self.text
+    else
+      return self.parent_node.render_to_text_after_node(node) + " " + self.text
+    end
   end
 
-  #def children
-  #  return self.nodes
+  def render_to_text
+    render_to_text_after_node(nil)
+  end
 
-  #end
+  def youngest_common_ancestor(other_node)
+    nodes = [self, other_node]
+    until nodes[0].id == nodes[1].id
+      oldest_node_index = nodes[0].created_at > nodes[1].created_at ? 0 : 1
+      nodes[oldest_node_index] = nodes[oldest_node_index].parent_node
+      if nodes[oldest_node_index].nil? # They have no common ancestor...
+        return nil
+      end
+    end
+    return nodes[0]
+  end
 
 end
