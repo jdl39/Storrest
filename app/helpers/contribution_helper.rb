fa713094908dabc8df33c18f4f84c5eda3e79eea
Jimmy Lee <jdl39@stanford.edu>
2014-05-22 11:13:28 -0700

    Slightly modified wordings.

--- app/helpers/contribution_helper.rb
+++ app/helpers/contribution_helper.rb
@@ -7,16 +7,16 @@ module ContributionHelper
 	end
 
 	def survey_questions
-		["How much does it advance the plot or make you feel something?",
+		["How much does it make you feel something?",
 		"How well-written is it?",
-		"How well does it fit with the rest of the story?",
+		"How well does it fit with the rest of the story or advance the plot?",
 		"How much do you like it overall?"]
 	end
 
 	def survey_question_summaries
-		["Emotional/Plot",
+		["Emotional",
 		"Style",
-		"Cohesion",
+		"Cohesion/Plot",
 		"Overall"]
 	end
 end
